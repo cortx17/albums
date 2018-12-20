a674d6e10432019c39270295cb47005cdacdebe7
cortx17 <pulsar.zero@gmail.com>
2018-12-20 18:41:42 +0000

    Updates

---
+++ precache-manifest.83bee0e474652b8f9711af10df4ab736.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0120ceb611b3553c9b40",
+    "url": "/albums/static/js/runtime~main.0120ceb6.js"
+  },
+  {
+    "revision": "4f8dd91fd71245b45ac3",
+    "url": "/albums/static/js/main.4f8dd91f.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/albums/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "4f8dd91fd71245b45ac3",
+    "url": "/albums/static/css/main.07260c57.chunk.css"
+  },
+  {
+    "revision": "4a777e6780894171d896bf387d419609",
+    "url": "/albums/index.html"
+  }
+];
